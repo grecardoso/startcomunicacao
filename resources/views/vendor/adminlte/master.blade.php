528acb5f80acc641fdab525150164cf3dd1513b7
Rodrigo Soares Souza <rodsoadev@gmail.com>
2017-10-09 23:28:30 -0300

    recompilando assets

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -31,11 +31,11 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 
-            <script type="text/javascript">
+            <script>
                 var _smartsupp = _smartsupp || {};
-                _smartsupp.key = '2de580083d5cb29a89a336f0234b1acac0aaf84d';
+                _smartsupp.key = 'YOUR_SMARTSUPP_CODE';
                 window.smartsupp||(function(d) {
-                    var s,c,o=smartsupp=function(){ o..push(arguments)};o.=[];
+                    var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
                     s=d.getElementsByTagName('script')[0];c=d.createElement('script');
                     c.type='text/javascript';c.charset='utf-8';c.async=true;
                     c.src='//www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
