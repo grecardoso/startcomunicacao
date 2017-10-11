c93a72025ffb669e812e7b9a44a661d2aef75500
Rodrigo Soares Souza <rodsoadev@gmail.com>
2017-10-11 01:14:16 -0300

    Alterações rápidas na deleção em massa de mensagens globais

--- resources/assets/js/controllers/messages/deleteAllGlobalMessages.js
+++ resources/assets/js/controllers/messages/deleteAllGlobalMessages.js
@@ -1,6 +1,7 @@
 function deleteAllGlobalMessages() {
     if ( confirm('Você tem certeza dessa ação?') ) {
         $('#delete-message-form').attr('action', '/global-messages');
+        $('#delete-message-form').attr('method', 'POST');
         $('#delete-message-form').submit();
     }
 }
